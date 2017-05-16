e469e416058c75bc7a2611672d4dad886e00abc1
jay <tuel@foxamil.com>
2017-05-16 10:40:37 +0800

    管理端提交订单、用户、财务三个控制器

---
+++ mall-manage-web/src/main/java/com/mall/controller/FinanceController.java
@@ -0,0 +1,16 @@
+package com.mall.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/finance")
+public class FinanceController {
+
+    private static final Logger logger = LoggerFactory.getLogger(FinanceController.class);
+
+
+    
+}
